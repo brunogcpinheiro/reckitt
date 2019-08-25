dc4ff2d6ed10e4e0321dc7fe5eeb6521efeb2d3f
Bruno Pinheiro <brunogcpinheiro@gmail.com>
2019-08-25 15:25:54 -0300

    initial commit w/ login page

---
+++ src/components/Login.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-content>
+    <div class="main--container">
+      <img src="@/assets/logo.png" alt="" />
+      <div class="main--title">
+        <h1>Login</h1>
+      </div>
+      <div class="main--form">
+        <v-form>
+          <v-text-field type="text" placeholder="usuario" />
+          <v-text-field type="password" placeholder="senha" />
+        </v-form>
+      </div>
+    </div>
+  </v-content>
+</template>
+
+<style lang="scss" scoped>
+.main--container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+  padding-top: 50px;
+  img {
+    width: 200px;
+    height: 120px;
+  }
+  h1 {
+    color: #db338f;
+    font-size: 2.2rem;
+    margin-top: 40px;
+  }
+}
+
+.main--form {
+  margin-top: 20px;
+}
+</style>
--- src/router.js
+++ src/router.js
@@ -1,8 +1,8 @@
-import Vue from "vue";
-import Router from "vue-router";
-import Home from "./views/Home.vue";
+import Vue from "vue"
+import Router from "vue-router"
+import Login from "./components/Login"
 
-Vue.use(Router);
+Vue.use(Router)
 
 export default new Router({
   mode: "history",
@@ -10,8 +10,8 @@ export default new Router({
   routes: [
     {
       path: "/",
-      name: "home",
-      component: Home
+      name: "login",
+      component: Login,
     },
     {
       path: "/about",
@@ -20,7 +20,7 @@ export default new Router({
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () =>
-        import(/* webpackChunkName: "about" */ "./views/About.vue")
-    }
-  ]
-});
+        import(/* webpackChunkName: "about" */ "./views/About.vue"),
+    },
+  ],
+})
