fd738b0a34d40ec250b2357d62fee6062dfb8d03
Bruno Pinheiro <brunogcpinheiro@hotmail.com>
2020-01-28 19:20:19 +0000

    fix babel to es6 modules

--- src/firebase/seed.js
+++ src/firebase/seed.js
@@ -1,19 +1,4 @@
-const firebase = require("firebase");
-// Required for side-effects
-require("firebase/firestore");
-
-// Initialize Cloud Firestore through Firebase
-firebase.initializeApp({
-  apiKey: "AIzaSyDyfr8-dmHZoGR7ehAcbOzj6wBC1hCNq0E",
-  authDomain: "reckitt-formulario.firebaseapp.com",
-  databaseURL: "https://reckitt-formulario.firebaseio.com",
-  projectId: "reckitt-formulario",
-  storageBucket: "",
-  messagingSenderId: "280111051036",
-  appId: "1:280111051036:web:2da2f5711d1c2637"
-});
-
-var db = firebase.firestore();
+import db from "./init";
 
 const stores = [
   {
